c3ccab8b5c836b3f947542f22fca15b7717ffd9e
Barrett Harber <barrett.harber@gmail.com>
2021-03-21 13:50:58 -0400

    Add angular material and navigation

--- web/quipt/src/app/app-routing.module.ts
+++ web/quipt/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [
+
+    MatToolbarModule,
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
