906242ca2d03040fa7ce0f3a6e2f35bb7ab58818
Barrett Harber <barrett.harber@gmail.com>
2021-07-18 16:52:56 -0400

    Add authguard to myboards

--- web/quipt/src/app/app-routing.module.ts
+++ web/quipt/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
-// import {AuthGuard} from '@auth0/auth0-angular';
+import {AuthGuard} from '@auth0/auth0-angular';
 import {HomeComponent} from './home/home.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import { BoardsComponent } from './boards/boards.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'myboards', component: BoardsComponent},
+  {path: 'myboards', component: BoardsComponent, canActivate: [AuthGuard]},
   {path: '404', component: NotFoundComponent},
   {path: '**', redirectTo: '/404'},
 ];
